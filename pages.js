ed3412645d726dcaf7d8e7f48cb69629221d917b
jimmysisenglath <jimmysisenglath@gmail.com>
2020-07-29 15:15:13 -0700

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/jimmysisenglath/jimmysisenglath.github.io.git";
+const repoURL = "https://jimmysisenglath.com/portfolio/";
 
 ghpages.publish(
   pathname,
