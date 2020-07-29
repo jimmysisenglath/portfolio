858bd1b61bfde3adbd2aedf15062b5581679e0a7
Jimmy Sisenglath <jimmysisenglath@gmail.com>
2020-07-29 15:46:51 -0700

    add deploy

---
+++ build/precache-manifest.c3876ad3d30424c4958227a749ded54b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283509a3561b3df0e24c941b9baae7c3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "92ab6c69e5c1a84d3fb0",
+    "url": "/portfolio/static/css/main.5f05969b.chunk.css"
+  },
+  {
+    "revision": "88d7d44951adbdd01cfc",
+    "url": "/portfolio/static/js/2.8fa335fd.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/portfolio/static/js/2.8fa335fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92ab6c69e5c1a84d3fb0",
+    "url": "/portfolio/static/js/main.2853e575.chunk.js"
+  },
+  {
+    "revision": "9c82abddd2577f6b4fb4",
+    "url": "/portfolio/static/js/runtime-main.e90eb3be.js"
+  },
+  {
+    "revision": "0c246fa08e8969160ef2f65ef1cc4776",
+    "url": "/portfolio/static/media/resume.0c246fa0.pdf"
+  }
+]);
